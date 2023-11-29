4fc286348f74155719b731b0f2e1a43951dcca9c
Robert Maina <robert-devs>
2023-11-29 12:08:23 +0300

    update on the animations

--- app/action.tsx
+++ app/action.tsx
@@ -1,9 +1,14 @@
 "use server";
 
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
 export const fetchAnime = async (page:number) =>{
     const response = await fetch(`https://shikimori.one/api/animes?${page}&limit=10&order=popularity`);
 
     const data = await response.json() 
 
-    return data
+    return data.map((item: AnimeProp, idx: number) => (
+          <AnimeCard key={item.id} anime={item} index={idx} />
+        ))
+    
 }
